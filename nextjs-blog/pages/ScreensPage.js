985cd7e7be90dda1678b211bdc3075e26950890d
roberts <roberts@arduino.lan>
2023-11-26 20:57:16 -0500

    YAML first cut

--- nextjs-blog/pages/ScreensPage.js
+++ nextjs-blog/pages/ScreensPage.js
@@ -6,26 +6,24 @@ import ScreensPageComponent from "../components/ScreensPageComponent";
 import ScreenData from "../src/ScreenData";
 import * as MainObjectType from "../src/MainObjectType";
 import { InferGetStaticPropsType, GetStaticProps } from "next";
-
+import Form from 'react-bootstrap/Form';
 
 const ScreensPage = () => {
   //let { data, setData } = useContext(MyContext);
   //const [data, setData] = useContext(MyContext);
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [yaml, setYaml] = useState(null);
 
   const handleDataUpdate = (updateData) => {
     console.log("update", updateData);
     let newData = Object.assign({}, updateData); 
-    setData({
-      ...newData,
-    });
+    setData({ ...newData, });
   };
 
 
   useEffect(() => {
-    if (null == data || data.length == 0) {
+    if (isLoading) {
       fetchData().then((data) => {
         console.log("fetch data", data.props.pages);
         if (isLoading) {
@@ -41,24 +39,30 @@ const ScreensPage = () => {
 
   const fetchData = async () => {
     console.log("pages start");
-    //const res = await fetch("http://localhost:3000/airgradient_esphome/display_sh1106_128_64.yaml");
-    const res = await fetch("https://robertsearle.github.io/airgradient_esphome/display_sh1106_128_64.yaml");
+    const res = await fetch("http://localhost:3000/display_sh1106_128_64.yaml");
+    //const res = await fetch("https://robertsearle.github.io/airgradient_esphome/display_sh1106_128_64.yaml");
     console.log("res", res);
     const body = await res.text();
     console.log("body", body);
-    const yaml = MainObjectType.readYamlFile(body);
-    console.log("yaml", yaml);
-    const pages = yaml[0].pages.map(
+    const newYaml = MainObjectType.readYamlFile(body);
+    console.log("newYaml", newYaml);
+    if (newYaml === null) {
+      console.error("Problem with the yaml", res);
+      return null;
+    }
+    const pages = newYaml[0].pages.map(
       (s) => new ScreenData(s.id, true, false, s)
     );
     console.log("pages", pages);
     if (isLoading) {
       setData(pages);
+      setYaml(newYaml);
       setIsLoading(false);
     }
     //let pagez = [new ScreenData("1", false, false, null)];
     return { props: { pages: pages } };
   };
+
 
   if (isLoading) {
     return (<p>Loading....</p>);
@@ -71,6 +75,15 @@ const ScreensPage = () => {
           onDataUpdate={handleDataUpdate}
         />
         <Link href="/Substitutions">Next</Link> 
+        <br/>
+        <Form.Group style={{width:"100%"}} >
+          <Form.Label>Replace the file <code>includes/display_sh1106_128_64.yaml</code></Form.Label><br/>
+          <Form.Control as="textarea" style={{width:"100%"}} rows="20">
+            {
+              MainObjectType.formatYamlData(yaml)
+            }
+          </Form.Control>
+        </Form.Group>
       </div>
     );
   }
