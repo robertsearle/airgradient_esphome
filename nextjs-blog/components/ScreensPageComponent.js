e6c5e9124e3dc8080b7e47b7b1a8bc889adab89d
roberts <roberts@arduino.lan>
2023-12-03 14:52:24 -0500

    Fixing Substitutions

--- nextjs-blog/components/ScreensPageComponent.js
+++ nextjs-blog/components/ScreensPageComponent.js
@@ -1,62 +1,83 @@
 // components/ScreensPageComponent.js
-import React, { useState } from 'react';
-import ScreenData from '../src/ScreenData';
-import * as MainObjectType from '../src/MainObjectType';
+import React, { useState } from "react";
+import ScreenData from "../src/ScreenData";
+import * as MainObjectType from "../src/MainObjectType";
 
-const ScreensPageComponent = ({ initialData, onDataUpdate }) => {
-  const [dataRows, setDataRows] = useState(initialData);
+const ScreensPageComponent = ({ props, onDataUpdate }) => {
+  console.log("ScreensPageComponent", props);
+
+  const [dataRows, setDataRows] = useState(props.pages);
 
   const handleCheckboxChange = (index, checkboxName) => {
-      if (dataRows == null) return;
-      let newData = Object.assign([], dataRows); 
-      if (checkboxName === 'show') {
-        newData[index].show = ! newData[index].show;
-      } else if (checkboxName === 'favorite') {
-        newData[index].favorite = !newData[index].favorite; 
+    if (dataRows == null || dataRows.length === 0) {
+      return;
+    } else {
+      let newData = Object.assign([], dataRows);
+      if (checkboxName === "show") {
+        newData[index].show = !newData[index].show;
+      } else if (checkboxName === "favorite") {
+        newData[index].favorite = !newData[index].favorite;
       } else {
         console.error("Error with check box problem", index, checkboxName);
       }
-      console.log ("components update", newData, index, checkboxName);
+      console.log("components update", newData, index, checkboxName);
       onDataUpdate(newData); // Call the callback to update the parent component
+    }
   };
 
   return (
-    <table >
-      <thead> 
+    <table>
+      <thead>
         <tr>
           <th>ID</th>
-          <th>Show</th>
-          <th>Favorite</th>
+          <th>
+            Show
+            <i className="text-xs">({dataRows.filter((e) => e.show).length})</i>
+          </th>
+          <th>
+            Favorite
+            <i className="text-xs">
+              ({dataRows.filter((e) => e.favorite).length})
+            </i>
+          </th>
           <th>Description</th>
         </tr>
       </thead>
       <tbody>
-        { dataRows != null && dataRows.map((dataRow, index) => (
-          <tr key={index}>
-            <td >
-              <label type="text" className="text-base" >{dataRow.id}</label>
-            </td>
-            <td >
-              <input type="checkbox" 
-                disabled={dataRow.id == 'boot' || dataRow.favorite}
-                checked={ (!dataRow.favorite && dataRow.show) || dataRow.favorite }
-                onChange={() => handleCheckboxChange(index, 'show')}
-              />
-            </td>
-            <td >
-              <input type="checkbox"
-                disabled={dataRow.id == 'boot'}
-                checked={dataRow.favorite}
-                onChange={() => handleCheckboxChange(index, 'favorite')}
-              />
-            </td>
-            <td >
-              <label type="text" className="text-[0.625rem] whitespace-pre"> 
-                    {MainObjectType.formatYamlData(dataRow.object)}
-              </label>
-            </td>
-          </tr>
-        ))}
+        {dataRows !== undefined &&
+          dataRows != null &&
+          dataRows.map((dataRow, index) => (
+            <tr key={index}>
+              <td>
+                <label type="text" className="text-base disabled">
+                  {dataRow.id}
+                </label>
+              </td>
+              <td>
+                <input
+                  type="checkbox"
+                  disabled={dataRow.id == "boot" || dataRow.favorite}
+                  checked={
+                    (!dataRow.favorite && dataRow.show) || dataRow.favorite
+                  }
+                  onChange={() => handleCheckboxChange(index, "show")}
+                />
+              </td>
+              <td>
+                <input
+                  type="checkbox"
+                  disabled={dataRow.id == "boot"}
+                  checked={dataRow.favorite}
+                  onChange={() => handleCheckboxChange(index, "favorite")}
+                />
+              </td>
+              <td>
+                <label type="text" className="text-[0.625rem] whitespace-pre">
+                  {MainObjectType.formatYamlData(dataRow.object)}
+                </label>
+              </td>
+            </tr>
+          ))}
       </tbody>
     </table>
   );
--- nextjs-blog/pages/ScreensPage.js
+++ nextjs-blog/pages/ScreensPage.js
@@ -5,43 +5,41 @@ import Link from "next/link";
 import ScreensPageComponent from "../components/ScreensPageComponent";
 import ScreenData from "../src/ScreenData";
 import * as MainObjectType from "../src/MainObjectType";
-//import { InferGetStaticPropsType, GetStaticProps } from "next";
+import { InferGetStaticPropsType, GetStaticProps } from "next";
 
 const ScreensPage = () => {
   //let { data, setData } = useContext(MyContext);
   //const [data, setData] = useContext(MyContext);
-  const [data, setData] = useState([]);
+  const [pages, setPages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [yaml, setYaml] = useState(null);
 
   const handleDataUpdate = (updateData) => {
     console.log("update", updateData);
     let newData = Object.assign([], updateData);
-    if (newData.filter(e => e.favorite).length > 3){
-      alert ("No more favorites can be selected");
+    if (newData.filter((e) => e.favorite).length > 3) {
+      alert("No more favorites can be selected");
     } else {
-      setData(newData);
+      setPages(newData);
     }
   };
 
-
   useEffect(() => {
     if (isLoading) {
-      fetchData().then((data) => {
-        console.log("fetch data", data.props.pages);
+      fetchPages().then((rawPages) => {
+        console.log("fetch pages", rawPages);
         if (isLoading) {
-          console.log("setting data", data.props.pages);
-          setData(data.props.pages);
+          console.log("setting pages", rawPages);
+          setPages(rawPages);
         }
       });
     } else {
-      return { props: {} };
+      return { pages };
     }
   }, []);
 
-
-  const fetchData = async () => {
-    const res = await fetch("https://robertsearle.github.io/airgradient_esphome/display_sh1106_128_64.yaml");
+  const fetchPages = async () => {
+    const res = await fetch("/airgradient_esphome/display_sh1106_128_64.yaml",);
     const body = await res.text();
     const newYaml = MainObjectType.readYamlFile(body);
     if (newYaml === null) {
@@ -49,66 +47,72 @@ const ScreensPage = () => {
       return null;
     }
     const pages = newYaml[0].pages.map(
-      (s) => new ScreenData(s.id, true, false, s)
+      (s) => new ScreenData(s.id, true, false, s),
     );
     if (isLoading) {
-      setData(pages);
+      setPages(pages);
       setYaml(newYaml);
       setIsLoading(false);
     }
-    //let pagez = [new ScreenData("1", false, false, null)];
-    return { props: { pages: pages } };
+    return pages;
   };
 
-
   const generateYamlFile = () => {
-     if (yaml === 0 || yaml.length == 0)
-       return "N/A";
-     let newYaml = Object.assign([], yaml);
-     console.log ("data", data);
-     const newPages = newYaml[0].pages.filter( e => {
-       const l = data.filter(e2  => e2.id == e.id);
-       if (l.length > 0) {
-         return l[0].show  && !l[0].favorite;
-       } else {
-         return false;
-       }
-     });
-     const favs = data.filter( e => e.favorite);
-     if (favs.length > 0) {
-       for (var i = newPages.length-1; i>= 0;  i--) {
-         for (var j = favs.length-1; j>=0; j--) {
-           let newData = Object.assign({}, favs[j].object);
-           newData.id = newData.id + "_" + i;
-           newPages.splice(i, 0, newData); 
-         }
-       }
-     }
-     newYaml[0].pages = newPages;
-     console.log("newYaml values", newYaml);
-     return MainObjectType.formatYamlData(newYaml)
-       .replaceAll(/^-/g, '  -')
-       .replaceAll(/\n/g, '\n  ');
-  }
-
+    if (yaml === 0 || yaml.length == 0) {
+      return "N/A";
+    }
+    let newYaml = Object.assign([], yaml);
+    console.log("pages", pages);
+    const newPages = newYaml[0].pages.filter((e) => {
+      const l = pages.filter((e2) => e2.id == e.id);
+      if (l.length > 0) {
+        return l[0].show && !l[0].favorite;
+      } else {
+        return false;
+      }
+    });
+    const favs = pages.filter((e) => e.favorite);
+    if (favs.length > 0) {
+      for (var i = newPages.length - 1; i >= 0; i--) {
+        for (var j = favs.length - 1; j >= 0; j--) {
+          let newPage = Object.assign({}, favs[j].object);
+          newPage.id = newPage.id + "_" + i;
+          newPages.splice(i, 0, newPage);
+        }
+      }
+    }
+    newYaml[0].pages = newPages;
+    console.log("newYaml values", newYaml);
+    return MainObjectType.formatYamlData(newYaml)
+      .replaceAll(/^-/g, "  -")
+      .replaceAll(/\n/g, "\n  ");
+  };
 
   if (isLoading) {
-    return (<p>Loading....</p>);
+    return <p>Loading....</p>;
   } else {
     return (
       <div>
         <h2>Screens Page</h2>
-        <ScreensPageComponent
-          initialData={data}
-          onDataUpdate={handleDataUpdate}
-        />
-        <Link href="/Substitutions">Next</Link> 
-        <br/>
-        <hr className="items-center justify-center  h-1 w-1/3 m-[5rem]  bg-gray-100 border-0 rounded dark:bg-gray-700"/>
-        <form className="md:w-1/2" >
-          <p>Replace the file <code className="text-teal-200 dark:text-teal-700" >includes/display_sh1106_128_64.yaml</code></p>
-          <br/>
-          <textarea readOnly as="textarea" className="textarea"  rows="10"  value={generateYamlFile()} />
+        <ScreensPageComponent props={{ pages }} onDataUpdate={handleDataUpdate} />
+        <Link href="/Substitutions">Next</Link>
+        <br />
+        <hr className="items-center justify-center  h-1 w-1/3 m-[5rem]  bg-gray-100 border-0 rounded dark:bg-gray-700" />
+        <form className="md:w-1/2">
+          <p>
+            Replace the file{" "}
+            <code className="text-teal-200 dark:text-teal-700">
+              includes/display_sh1106_128_64.yaml
+            </code>
+          </p>
+          <br />
+          <textarea
+            readOnly
+            as="textarea"
+            className="textarea"
+            rows="10"
+            value={generateYamlFile()}
+          />
         </form>
       </div>
     );
